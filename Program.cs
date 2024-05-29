4df58a52b6431a477f9ac8e106480500f269ec8a
Canh Thien <canhthien91@gmail.com>
2024-05-29 22:10:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap ngay thang nam sinh (yyyy-mm-dd): ");
+        DateTime birthDay = DateTime.ParseExact(Console.ReadLine(), "yyyy-MM-dd", null);
+
+        int age = DateTime.Now.Year - birthDay.Year;
+        if (DateTime.Now < birthDay.AddYears(age)) age--;
+
+        Console.WriteLine("Tuoi cua ban la: " + age);
+    }
+}
